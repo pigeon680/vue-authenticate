e81661c79b2600cf50e4207d78dc9e1c7b9368f9
WhitePigeon777 <whitepigeon0303@gmail.com>
2025-02-05 06:04:49 -0500

    fixed

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,14 +10,14 @@
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
-      <li>
+      <!-- <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
           target="_blank"
           rel="noopener"
           >babel</a
         >
-      </li>
+      </li> -->
       <li>
         <a
           href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
